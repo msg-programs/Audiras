b68a668398772cee32d37ca84d91bf70fe20ee41
Clemens C <clemeli@web.de>
2020-05-09 19:05:42 +0200

    Formatted Main

--- src/main/RadioMain.java
+++ src/main/RadioMain.java
@@ -10,19 +10,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
 
 import gui.Window;
 import settings.Lang;
 import settings.Settings;
 import streamlogic.RecordingMaster;
-import streamlogic.StreamList;
+import streamlogic.StationList;
 
 public class RadioMain {
 
@@ -30,23 +30,31 @@ public class RadioMain {
 
 	public static void main(String[] args) {
 
-		
+//try {
+//	test();
+//} catch (MalformedURLException e) {
+//	// TODO Auto-generated catch block
+//	e.printStackTrace();
+//} catch (IOException e) {
+//	// TODO Auto-generated catch block
+//	e.printStackTrace();
+//}
+
 		Settings.init();
 		System.out.println("s init");
 		Lang.init();
 		System.out.println("l init");
+		StationList.init();
+		System.out.println("sl init");
 		RecordingMaster.init();
 		System.out.println("rm init");
-		StreamList.init();
-		System.out.println("sl init");
-		
-		initTrayIcon();
 
+		initTrayIcon();
 
 		if (Settings.getInstRec()) {
 			RecordingMaster.setAllOn();
 		}
-		
+
 		if (Settings.getShowWin()) {
 			win = new Window();
 		}
@@ -60,91 +68,71 @@ public class RadioMain {
 	}
 
 	private static void initTrayIcon() {
-			TrayIcon ti = null;
+		TrayIcon ti = null;
 
-			if (SystemTray.isSupported()) {
-				SystemTray t = SystemTray.getSystemTray();
+		if (SystemTray.isSupported()) {
+			SystemTray t = SystemTray.getSystemTray();
 
-				Image i = null;
-				try {
-					i = ImageIO.read(Settings.ICO);
-				} catch (IOException e1) {
-					System.err.println("Couldn't load the tray icon image!");
-					e1.printStackTrace();
-				}
+			Image i = null;
+			try {
+				i = ImageIO.read(Settings.ICO);
+			} catch (IOException e1) {
+				System.err.println("Couldn't load the tray icon image!");
+				e1.printStackTrace();
+			}
 
-				MenuItem open = new MenuItem("Open window");
-				MenuItem close = new MenuItem("Exit Audiras");
+			MenuItem open = new MenuItem("Open window");
+			MenuItem close = new MenuItem("Exit Audiras");
 
-				ActionListener al = new ActionListener() {
-					public void actionPerformed(ActionEvent ae) {
-						if (ae.getSource().equals(open)) {
-							if (win == null) {
-								win = new Window();
-							}
-						}
-						if (ae.getSource().equals(close)) {
-							System.exit(0);
+			ActionListener al = new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					if (ae.getSource().equals(open)) {
+						if (win == null) {
+							win = new Window();
 						}
 					}
-				};
-
-				PopupMenu popup = new PopupMenu();
-
-				open.addActionListener(al);
-				popup.add(open);
-
-				close.addActionListener(al);
-				popup.add(close);
-
-				ti = new TrayIcon(i, "Audiras", popup);
-				ti.addActionListener(al);
-
-				try {
-					t.add(ti);
-				} catch (AWTException e) {
-					System.err.println("Couldn't create the tray icon:");
-					e.printStackTrace();
+					if (ae.getSource().equals(close)) {
+						System.exit(0);
+					}
 				}
-			}
-	}
-	
-	public static void test() {
-		String url = "http://208.113.165.40:8000/stream.mp3";
-		
-		try {
-			URLConnection conn;
-			conn = new URL(url).openConnection();
-			conn.setRequestProperty("Icy-MetaData", "1");
-			conn.setRequestProperty("Connection", "close");
-			conn.setRequestProperty("Accept", null);
-			conn.connect();
-			
-			Map<String, List<String>> hmap=  conn.getHeaderFields();
-			for (String s : hmap.keySet()) {
-				System.out.print(s+": ");
-				String.join("\n", hmap.get(s));
-				System.out.println();
-			}
-			
-			System.out.println("######");
-			
-			InputStream is = conn.getInputStream();
-			byte[] buf = new byte[16255];
-			is.readNBytes(buf, 0, 16255);
-			System.out.println("######");
-			System.out.println(buf[16000]*16);
-			System.out.println("######");
+			};
 
-			
-			System.out.println(new String(buf));
-			
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+			PopupMenu popup = new PopupMenu();
+
+			open.addActionListener(al);
+			popup.add(open);
+
+			close.addActionListener(al);
+			popup.add(close);
+
+			ti = new TrayIcon(i, "Audiras", popup);
+			ti.addActionListener(al);
+
+			try {
+				t.add(ti);
+			} catch (AWTException e) {
+				System.err.println("Couldn't create the tray icon:");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void test() throws MalformedURLException, IOException {
+		String url = "url here";
+
+		URLConnection conn;
+		conn = new URL(url).openConnection();
+		conn.setRequestProperty("Icy-MetaData", "1");
+		conn.setRequestProperty("Connection", "close");
+		conn.setRequestProperty("Accept", null);
+		conn.connect();
+
+		Map<String, List<String>> hmap = conn.getHeaderFields();
+		for (String s : hmap.keySet()) {
+			System.out.print(s + ": ");
+			hmap.get(s).forEach(val -> System.err.println(val));
+			System.out.println();
 		}
 		System.exit(0);
 	}
-
 }
