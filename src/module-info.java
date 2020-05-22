0b248ac3fc9932f139952f6873226552f979c5d1
Clemens C <clemeli@web.de>
2020-05-22 21:08:48 +0200

    Added module info

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module audiras {
+	exports com.msgprograms.audiras.main;
+	exports com.mpatric.mp3agic;
+	exports com.msgprograms.audiras.streamlogic;
+	exports com.msgprograms.audiras.gui;
+	exports com.msgprograms.audiras.settings;
+
+	requires java.desktop;
+}
